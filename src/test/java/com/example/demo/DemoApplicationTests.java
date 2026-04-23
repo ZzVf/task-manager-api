8f8157a4b002dfb28665d944cbbc057d89d0c0d0
ZzVf <goldtime79.1979@gmail.com>
2026-04-23 16:47:47 +0200

    Add unit test for TaskService

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,23 @@
 package com.example.demo;
 
+import com.example.demo.exception.TaskNotFoundException;
+import com.example.demo.service.TaskService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 @SpringBootTest
-class DemoApplicationTests {
+class TaskServiceTest {
+
+	@Autowired
+	private TaskService taskService;
 
 	@Test
-	void contextLoads() {
+	void shouldThrowExceptionWhenTaskNotFound() {
+		assertThrows(TaskNotFoundException.class, () -> {
+			taskService.getById(999L);
+		});
 	}
-
 }
